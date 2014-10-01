85b15aabc7b81fd1e0635304fced195dfddaaee8
maybelinot <edik.trott@yandex.ru>
2014-10-01 19:29:29 +0200

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ VUT_project
 ===========
 
 De novo identification of LTR retrotransposons
+
+==========
+Python 3.4
+Biopython 1.64
--- run.py
+++ run.py
@@ -1,6 +1,38 @@
 __author__ = 'Eduard Trott'
-print('hmm')
+
+from Bio import SeqIO
+
+FILENAME = "Homo sapiens chromosome X genomic scaffold, GRCh38 Primary Assembly HSCHRX_CTG3.fasta"
 
 
-def somefunc():
-    pass
+class GenomeClass:
+    """
+    Class to represent genome
+    """
+    def __init__(self, filename):
+        handle = open(filename, "rU")
+        self.data = list(SeqIO.parse(handle, "fasta"))
+
+    def __str__(self):
+        """
+        Human readable representation of genome
+        """
+    def run(self):
+        self.de_novo(self)
+
+    @staticmethod
+    def de_novo(self):
+        """
+        De novo identification of young intact LTR retroelements
+        """
+        def de_novo_first_step(name):
+            """
+
+            """
+            print(name)
+        de_novo_first_step('Vasya')
+
+genome = GenomeClass(FILENAME)
+genome.de_novo(genome)
+# self.data[0].seq
+
