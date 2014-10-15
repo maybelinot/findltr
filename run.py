d78edc83a134221132f7935ce6ab0b63cdc90dd6
maybelinot <edik.trott@yandex.ru>
2014-10-15 22:14:08 +0200

    pattern searching (update) forming of LTRs from LTR parts

--- run.py
+++ run.py
@@ -96,10 +96,12 @@ class GenomeClass:
         """
 
     def run(self):
-        # self.de_novo(self)
-        db = shelve.open('LCP.db', writeback=True)
-        seq = str(self.data.seq)
-        [print(seq[db['young_lcp'][idx][0]:db['young_lcp'][idx][0]+40], seq[db['young_lcp'][idx][1]:db['young_lcp'][idx][1]+40]) for idx in range(15)]
+        self.de_novo(self)
+        # db = shelve.open('LCP.db', writeback=True)
+        # seq = str(self.data.seq)
+        # [print(seq[db['young_lcp'][idx][0]:db['young_lcp'][idx][0]+40], seq[db['young_lcp'][idx][1]:db['young_lcp'][idx][1]+40]) for idx in range(15)]
+        # print(len(db['young_lcp']))
+        # db.close()
 
     @staticmethod
     def de_novo(self):
@@ -110,28 +112,29 @@ class GenomeClass:
 
         # de_novo_first_step(binary searching with LCP array)
         min_pattern_len = 40
-        min_distances = 1000
-        max_distances = 20000
+        min_distance = 1000
+        max_distance = 20000
 
         seq = str(self.data.seq)
-        # seq = 'kakoe-to soobcheniekakoe-to s$'
-        start_time = time.time()
-        # print(seq)
-        # print(suffix_array)
-        # print(lcp)
-        output = []
-        for idx in range(len(seq) - (min_pattern_len*2 + min_distances)):
-            pattern = seq[idx:idx + min_pattern_len]
-            if not 'N' in pattern:
-                text = seq[idx + min_pattern_len + min_distances:idx + min_pattern_len + min_distances + max_distances]
-                ans = None
-                if pattern in text:
-                    output.append([idx, text.index(pattern) + idx + min_pattern_len + min_distances])
-            # print(idx, ' из ', len(seq) - (min_pattern_len*2 + min_distances))
-        print("--- %s seconds ---" % (time.time() - start_time))
-        db = shelve.open('LCP.db', writeback=True)
-        db['young_lcp'] = output
-        db.close()
+        # start_time = time.time()
+        # output = []
+        # idx = 0
+        # while idx < len(seq) - (min_pattern_len*2 + min_distance):
+        #     pattern = seq[idx:idx + min_pattern_len]
+        #     if not 'N' in pattern:
+        #         text = seq[idx + min_pattern_len + min_distance:idx + min_pattern_len + min_distance + max_distance]
+        #         ans = None
+        #         if pattern in text:
+        #             idx += min_pattern_len
+        #             output.append([idx, text.index(pattern) + idx + min_pattern_len + min_distance])
+        #     idx += 1
+        # print("--- %s seconds ---" % (time.time() - start_time))
+        # db = shelve.open('LCP.db', writeback=True)
+        # # db = [[start_of_pattern, start_of_appropriate_pattern], ...] where pattern has a length equal min_pattern_len
+        # # and distance between patterns is in range (min_distance : max_distance)
+        # db['young_lcp_parts'] = output
+        # db.close()
+
         ################################################################################################################
         # output = []
         # for idx in range(len(seq) - (min_pattern_len*2 + min_distances)):
@@ -153,12 +156,26 @@ class GenomeClass:
         # print(output)
         ################################################################################################################
         # de_novo_second_step
+        max_ltr_len = 1000
+        db = shelve.open('LCP.db', writeback=True)
+
+        groups_of_ltrs = [[[db['young_lcp_parts'][0][0], db['young_lcp_parts'][0][0] + min_pattern_len], [db['young_lcp_parts'][0][1], db['young_lcp_parts'][0][1] + min_pattern_len]]]
+
+        for lcp_part in db['young_lcp_parts'][1:]:
+            if lcp_part[0] + min_distance < groups_of_ltrs[-1][1][0] and ((lcp_part[0] - groups_of_ltrs[-1][0][1] < max_ltr_len) or (lcp_part[1] - groups_of_ltrs[-1][1][1] < max_ltr_len)):
+                groups_of_ltrs[-1][0][1] = lcp_part[0] + min_pattern_len
+                groups_of_ltrs[-1][1][1] = lcp_part[1] + min_pattern_len
+            else:
+                groups_of_ltrs.append([[lcp_part[0], lcp_part[0] + min_pattern_len], [lcp_part[1], lcp_part[1] + min_pattern_len]])
+        db['young_lcp'] = groups_of_ltrs
+        db.close()
+        # print(seq[groups_of_ltrs[1][0][0]:groups_of_ltrs[1][0][1]], '\n', seq[groups_of_ltrs[1][1][0]:groups_of_ltrs[1][1][1]])
+
+        # if the distance is less than 1000 then consider this as a duplicates?
+
 
         # de_novo_last_step
 
 genome = GenomeClass(FILENAME)
 genome.run()
-
-# print(genome.__x)
-# self.data[0].seq
 
